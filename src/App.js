af95d9db839499c4c24b07e140e5ba933cdf178b
Birdyo <a9650615@gmail.com>
2016-08-04 03:19:51 +0800

    修正 style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import MenuIcon from 'material-ui/lib/svg-icons/navigation/menu';
 
 export default React.createClass({
   render() {
-    
+    console.log(process);
     let styles = {
-      AppBar: {
-        'max-width': '1080px',
-        'margin': 'auto',
-        'background': '#D81B60'
+      root: {
+        background: "#D81B60",
+        padding: "10px 5px"
+      },
+      title: {
+        color: "#424242",
+        fontSize: "1.2rem",
+        lineHeight: "60px"
+      },
+      iconLeft: {
+        color: "#424242"
+      },
+      appBar: {
+        maxWidth: "1080px",
+        margin: "auto",
+        background: "#fff",
+        padding: "0px 20px",
+        minHeight: "auto"
       }
     };
-    console.log(process);
+
     return (
-      <div>
+      <div style={styles.root}>
         <AppBar
-          title={process.env.WEBSITE_TITLE}
-          iconElementLeft={<div />}
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          style={styles.AppBar}
+          title={"未命名的漫畫前端"}
+          iconElementLeft={<IconButton><MenuIcon color={styles.iconLeft.color}/></IconButton>}
+          style={styles.appBar}
+          titleStyle={styles.title}
         />
         {this.props.children}
       </div>
     )
   }
+
 });
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import GridList from 'material-ui/lib/grid-list/grid-list';
 
 export default class Home extends Component {
   render() {
+    const styles= {
+
+    };
+    
     return (
       <div>
-        Hello, world!
+        
       </div>
     );
   }
+
 }
