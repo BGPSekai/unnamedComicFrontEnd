cfcf593fa0c012e1e3cf0f7c470b961d39f47065
Birdyo <a9650615@gmail.com>
2016-08-04 02:14:43 +0800

    新增 AppBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import AppBar from 'material-ui/lib/app-bar';
 
 export default React.createClass({
   render() {
+    
+    let styles = {
+      AppBar: {
+        'max-width': '1080px',
+        'margin': 'auto',
+        'background': '#D81B60'
+      }
+    };
+    console.log(process);
     return (
       <div>
+        <AppBar
+          title={process.env.WEBSITE_TITLE}
+          iconElementLeft={<div />}
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          style={styles.AppBar}
+        />
         {this.props.children}
       </div>
     )
