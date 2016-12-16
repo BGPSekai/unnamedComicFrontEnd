4109458010b5ed510902024f5716e324f5ffc7db
WeiZhiHuang <s1410432005@gms.nutc.edu.tw>
2016-12-16 16:17:54 +0800

    fix merge bug

--- src/views/Comic/ChapterPage.js
+++ src/views/Comic/ChapterPage.js
@@ -162,7 +162,6 @@ class ChapterPage extends Component {
               }
             </div>
           </div>
-<<<<<<< HEAD
         </Container>
         <div style={ChapterPageStyle.secendSection}>
           <Container style={ChapterPageStyle.secendSectionContainer}>
@@ -170,8 +169,6 @@ class ChapterPage extends Component {
           </Container>
         </div>
         <Container style={ChapterPageStyle.chapterSelectContainer}>
-=======
->>>>>>> develop
           {
             this.props.backEnd &&
             <FloatingActionButton
@@ -185,16 +182,6 @@ class ChapterPage extends Component {
               <ContentAdd />
             </FloatingActionButton>
           }
-<<<<<<< HEAD
-=======
-        </Container>
-        <div style={ChapterPageStyle.secendSection}>
-          <Container style={ChapterPageStyle.secendSectionContainer}>
-            <FlatButton label="檢舉" backgroundColor={ChapterPageStyle.secendSectionButton.background} />
-          </Container>
-        </div>
-        <Container style={ChapterPageStyle.chapterSelectContainer}>
->>>>>>> develop
           {
             UserModule.checkIsLogin() &&
             <ComicFavorite style={ChapterPageStyle.addChapter} comicId={this.state.comicData.id} />
