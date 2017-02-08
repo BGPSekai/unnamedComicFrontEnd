84e1932cc630ae1b0ce780adfa5b44c6b541d8f1
Birdyo <a9650615@gmail.com>
2017-02-08 20:49:36 +0800

    捨去 webpack-dashboard
     - webpack-dashboard

--- devServer.js
+++ devServer.js
@@ -1,13 +1,13 @@
 var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
-var DashboardPlugin = require('webpack-dashboard/plugin');
+//var DashboardPlugin = require('webpack-dashboard/plugin');
 var config = require('./webpack.config.dev');
 
 var app = express();
 var compiler = webpack(config);
 
-compiler.apply(new DashboardPlugin());
+//compiler.apply(new DashboardPlugin());
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
